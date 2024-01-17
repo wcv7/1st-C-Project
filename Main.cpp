eca7c48f35bd99c758e2f30a246950f85ca8cdd6
wcv7 <150534109+wcv7@users.noreply.github.com>
2024-01-17 21:08:14 +0000

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <array>
+#include <string>
 using namespace std;
 
-int main(){
-    cout << "Hello World!" << endl;
+int numbers[] = {1,2,3,4,5};
+bool found = false;
+int number;
+string output;`
+int main()
+{
+    cout << "Hello Oscar!";
+    cout << "\nEnter A Number: ";
+    cin >> number;
+    cout << "Number is: " << number;
+    for(auto i: numbers)
+    {
+        if(number == numbers[i])
+        {
+            found = true;
+        }
+    }
+    if(found == true)
+    {
+        cout << "\n"+to_string(number)+" Was Found!";
+    }
+    else
+    {
+        cout << "\n"+to_string(number)+" Was Not Found!";
+    }
     return 0;
 }
